29bb324cdc8b9d24d8e6856ff355cebe56d57fc0
khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2025-05-13 22:17:29 +0600

    Added necessary packages, main bottom navigation screen, book screen

--- lib/core/layout/main_bottom_navigation_screen.dart
+++ lib/core/layout/main_bottom_navigation_screen.dart
@@ -1,10 +1,46 @@
+import 'package:al_hadith/features/books/presentation/book_screen.dart';
 import 'package:flutter/material.dart';
 
-class MainBottomNavigationScreen extends StatelessWidget {
-  const MainBottomNavigationScreen({super.key});
+class MainBottomNavigationScreen extends StatefulWidget {
+  const MainBottomNavigationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainBottomNavigationScreen> createState() =>
+      _MainBottomNavigationScreenState();
+}
+
+class _MainBottomNavigationScreenState
+    extends State<MainBottomNavigationScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const BookScreen(),
+    const BookScreen(),
+    const BookScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Chapters',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }
